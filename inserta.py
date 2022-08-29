4ceefdda00d64280a1ae9b8b2bf6c7b763e8e9ed
Pedro Trejo <111399899+Pedrotc13@users.noreply.github.com>
2022-08-28 22:37:11 -0500

    Update inserta.py

--- inserta.py
+++ inserta.py
@@ -1,2 +1,7 @@
+import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
